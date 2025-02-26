4a49f629031d307f7f7d92a4c5e73230d58474d9
lag <lag@lag>
2025-02-27 04:53:54 +0530

    adding rag groq

---
+++ raggroq.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+import streamlit as st
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+
+os.environ["HF_TOKEN"]=os.getenv("HF_TOKEN")
+embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
